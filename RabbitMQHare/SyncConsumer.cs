1684073a3f9d92951812f903c78b20f0e295b505
Manu Cupcic <m.cupcic@criteo.com>
2013-06-21 10:59:05 +0200

    Add a synchronous consumer.
    Useful when you know that your OnMessage listener is super super
    quick, or already makes things asynchronous.

---
+++ RabbitMQHare/SyncConsumer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQHare
+{
+    /// <summary>
+    /// A class that dispatches messages synchronously.
+    /// 
+    /// That means the OnMessage event should be connected only to things
+    /// that return _really_ quickly.
+    /// </summary>
+    public class SyncConsumer : BaseConsumer
+    {
+        public SyncConsumer(IModel model, bool autoAck)
+            : base(model, autoAck)
+        {
+            Model.BasicQos(0, 1, false);
+        }
+
+        protected override void ProcessOne(BasicDeliverEventArgs e)
+        {
+            try
+            {
+                DispatchMessage(e);
+            }
+            catch (Exception exception)
+            {
+                DispatchError(e, exception);
+            }
+        }
+    }
+}
